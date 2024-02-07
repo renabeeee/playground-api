11e184fd7c109b8e8c59a15e7c6a84417820980c
Serena <serenafgreen@gmail.com>
2024-02-06 21:36:22 -0800

    updates the rsvp jbuilder to include user's profile image

--- app/views/rsvps/_rsvp.json.jbuilder
+++ app/views/rsvps/_rsvp.json.jbuilder
@@ -1,6 +1,9 @@
 json.id rsvp.id
 json.game rsvp.game
 json.game rsvp.game
-json.user rsvp.game.user.first_name
+json.user do
+  json.first_name rsvp.game.user.first_name
+  json.profile_image rsvp.game.user.profile_image
 json.created_at rsvp.created_at
 json.updated_at rsvp.updated_at
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_043857) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_07_050329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -118,7 +118,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_07_043857) do
     t.string "location"
     t.string "birthdate"
     t.string "experience"
-    t.string "profile_image"
+    t.string "profile_image", default: "https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2281862025.jpg"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
