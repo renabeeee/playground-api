0a257769e80ed502084fdde4da64f75f31d3a116
Serena <serenafgreen@gmail.com>
2024-02-22 20:57:09 -0800

    creates method in attendance model to keep track of attendance count for each event

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,15 @@ class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
   has_many :users
+
+  after_create :update_attendance_count
+  after_destroy :update_attendance_count
+
+  validates :user_id, uniqueness: { scope: :event_id }
+
+  private
+
+  def update_attendance_count
+    event.update_attendance_count
+  end
 end
